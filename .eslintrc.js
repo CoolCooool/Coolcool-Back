e954905403ab50e615e528e0d45855b69048ce1b
yback1223 <yoyob1223@gmail.com>
2023-03-28 16:26:47 +0900

    feat: #10 Init project setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,23 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-	  project: 'tsconfig.json',
-	  tsconfigRootDir: __dirname,
-	  sourceType: 'module',
-	},
-	plugins: [
-	  'import',
-	  '@typescript-eslint/eslint-plugin',
-	],
-	extends: [
-	  'airbnb-typescript/base',
-	],
-	root: true,
-	env: {
-	  node: true,
-	  jest: true,
-	},
-	ignorePatterns: ['.eslintrc.js'],
-	rules: {
-	  '@typescript-eslint/interface-name-prefix': 'off',
-	  '@typescript-eslint/explicit-function-return-type': 'off',
-	  '@typescript-eslint/explicit-module-boundary-types': 'off',
-	  '@typescript-eslint/no-explicit-any': 'off',
-	},
-  };
-  
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', '@typescript-eslint', 'import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended', 'airbnb-typescript'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'react/jsx-filename-extension': [0, {}],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
