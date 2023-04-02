213acb24203c458adec2dcc0d99163f312977374
JiniEun <julietjin94@gmail.com>
2023-04-03 05:16:35 +0900

    feat: #46 app.module.ts에 typeorm추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from '../cool-config/back/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeORMConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
