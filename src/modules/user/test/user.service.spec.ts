9d835ce3405f4d16eb302100516792624ccf44ba
JiniEun <julietjin94@gmail.com>
2023-04-10 10:44:40 +0900

    feat: #24 user test 수정(user.service.spec.ts)

--- src/modules/user/test/user.service.spec.ts
+++ src/modules/user/test/user.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from '@root/modules/user/user.service';
+import { getPgTestTypeTrmModule } from '@root/common/config/get-pg-test-type-trm-module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@root/modules/user/entity/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [getPgTestTypeTrmModule(), TypeOrmModule.forFeature([User])],
       providers: [UserService],
     }).compile();
 
