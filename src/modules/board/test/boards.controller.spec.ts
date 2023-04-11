cf50957973ca6b5ff9d087b27ac052fceaa520e5
miffyKing <whqudghk28@gmail.com>
2023-04-12 01:06:40 +0900

    feat: #54 커뮤니티 API 테스트코드 작성

--- src/modules/board/test/boards.controller.spec.ts
+++ src/modules/board/test/boards.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardsController } from '../boards.controller';
 import { BoardsService } from '../boards.service';
-
+import { getPgTestTypeTrmModule } from '@root/common/config/get-pg-test-type-trm-module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from '@root/modules/board/entity/board.entity';
+import { instance, mock, when } from 'ts-mockito';
 describe('BoardsController', () => {
   let controller: BoardsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [getPgTestTypeTrmModule(), TypeOrmModule.forFeature([Board])],
       controllers: [BoardsController],
       providers: [BoardsService],
     }).compile();
@@ -16,5 +20,22 @@ describe('BoardsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('boards', () => {
+    it('findBoardById', async () => {
+      const id = 1;
+      const board: Board = new Board();
+      board.id = id;
+
+      const stubBoardApiService: BoardsService = mock(BoardsService);
+      when(stubBoardApiService.findAll()).thenResolve([board]);
+
+      controller = new BoardsController(instance(stubBoardApiService));
+
+      const boards: Board[] = await controller.getBoards();
+      expect(boards).toHaveLength(id);
+      expect(boards[0].id).toBe(id);
+    });
   });
 });
--- src/modules/board/test/boards.service.spec.ts
+++ src/modules/board/test/boards.service.spec.ts
@@ -1,11 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { BoardsService } from '../boards.service';
+import { BoardsService } from '@root/modules/board/boards.service';
+import { getPgTestTypeTrmModule } from '@root/common/config/get-pg-test-type-trm-module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from '@root/modules/board/entity/board.entity';
 
 describe('BoardsService', () => {
   let service: BoardsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [getPgTestTypeTrmModule(), TypeOrmModule.forFeature([Board])],
       providers: [BoardsService],
     }).compile();
 
