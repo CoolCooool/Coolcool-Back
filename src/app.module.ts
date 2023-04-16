ba1f1b1b1676c968eb6b50d5081cd14f409ac893
Park-Jeong-Ki <71398226+Park-Jeong-Ki@users.noreply.github.com>
2023-04-17 03:14:04 +0900

    Feature/cool #32 (#75)
    * feat: #32 git rebase 환경설정
    
    * feat: #32 git rebase
    
    * feat: #32 rebase continue
    
    * feat: #32 git rebase
    
    * feat: #32 DTO 작성 및 Postgre 스키마 확인
    
    * feat: #32 create 성공
    
    * feat: #32 http 파일 추가, find By Id 구현
    
    * feat: #32 http 파일 변경
    
    * feat: #32 SOFT DELETE 구현
    
    * feat: #32 UPDATE 구현은 일단 추후에 하기로 함 (구현이 어려움)
    
    * feat: #32 rebase 삭제
    
    * feat: #32 CRUD 완성
    
    * feat: #32 camelCase 변경
    
    * feat: #32 camelCase, isDeleted Default false
    
    * feat: #32 camelCase, isDeleted Default false
    
    * feat: #32 try 문 안에 await으로 변경
    
    * feat: #32 PR 병합 충돌 해결
    
    ---------
    
    Co-authored-by: JiniEun <julietjin94@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,10 @@ import { typeORMConfig } from '../cool-config/back/typeorm.config';
 import { UserModule } from '@root/modules/user/user.module';
 import { OrderModule } from './modules/orders/orders.module';
 import { BoardsModule } from '@root/modules/board/boards.module';
+import { ChatGptAiModule } from './modules/chat-gpt-ai/chat-gpt-ai.module';
+
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, BoardsModule, OrderModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UserModule, BoardsModule, OrderModule, ChatGptAiModule],
   controllers: [AppController],
   providers: [AppService],
 })
